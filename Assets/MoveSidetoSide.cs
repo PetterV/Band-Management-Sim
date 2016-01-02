81614556531327842c80d4965579236aff7cae0b
PetterV <Petter@Petters-MacBook-Air.local>
2016-01-02 15:18:57 +0100

    Startet

---
+++ Assets/MoveSidetoSide.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSidetoSide : MonoBehaviour {
+	private float newPositionX;
+	public float moveSpeed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("left")){
+			newPositionX = transform.position.x-moveSpeed;
+			transform.position = new Vector3 (newPositionX, 0, 0);
+		}
+		if (Input.GetKey("right")){
+			newPositionX = transform.position.x+moveSpeed;
+			transform.position = new Vector3 (newPositionX, 0, 0);
+		}
+	}
+}
