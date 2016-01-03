0e5162e31b77621e74c5882728ab335b789752a9
Kjetil Bugge Kristoffersen <kjetilbk@ifi.uio.no>
2016-01-03 20:56:50 +0100

    Fiksa minidelay på stair-use

--- Assets/Scripts/MoveSidetoSide.cs
+++ Assets/Scripts/MoveSidetoSide.cs
@@ -5,7 +5,9 @@ public class MoveSidetoSide : MonoBehaviour {
 	private float newPositionX;
 	private float newPositionY;
 	public float moveSpeed = 0.5f;
-	public int currentFloor = 1;    
+	public int currentFloor = 1;
+	public float stairUseDelay = 0.1f;
+	private float timeSinceLastStairUse = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -22,19 +24,19 @@ public class MoveSidetoSide : MonoBehaviour {
 			newPositionX = transform.position.x+moveSpeed;
 			transform.position = new Vector3 (newPositionX, transform.position.y, transform.position.z);
 		}
+		timeSinceLastStairUse += Time.deltaTime;
 	}
 
 	void OnTriggerStay(Collider coll){
+		if (coll.gameObject.tag == "Stairs" && (Input.GetKeyUp("up") || Input.GetKeyUp("down"))) {
+			if (timeSinceLastStairUse > stairUseDelay) {
+				if (Input.GetKeyUp ("up"))
+					coll.gameObject.SendMessage ("MoveUp", this.gameObject);
+				else if (Input.GetKeyUp ("down"))
+					coll.gameObject.SendMessage ("MoveDown", this.gameObject);
+				timeSinceLastStairUse = 0f;
+			}
+		}
 
-        //print("whatever egentlig");
-		if(coll.gameObject.tag == "Stairs" && Input.GetKeyUp("up") ){
-			coll.gameObject.SendMessage ("MoveUp", this.gameObject);
-          //  print("lolol");
-			//currentFloor = currentFloor + 1;
-		}
-		if(coll.gameObject.tag == "Stairs" && Input.GetKeyUp("down") ){
-			coll.gameObject.SendMessage ("MoveDown", this.gameObject);
-			//currentFloor = currentFloor - 1;
-		}
 	}
 }
