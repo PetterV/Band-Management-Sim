71b671f3d049fd4092693d5d52da10f18b5793b9
Thomas Skuggevik <thomsku@stud.ntnu.no>
2016-01-03 17:57:04 +0100

    whack sound and machinesound script added

---
+++ Assets/MachineSound.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MachineSound : MonoBehaviour {
+
+    public AudioClip audioClip;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	
+	}
+
+    void OnTriggerStay(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player" && Input.GetKeyDown("space"))
+        {
+            GetComponent<AudioSource>().Play();
+
+        }
+    }
+}
