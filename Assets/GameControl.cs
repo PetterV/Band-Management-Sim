cbbb0a01b5b1d0cdbd1fca5672f804dc76d8b77d
Kjetil Bugge Kristoffersen <kjetilbk@ifi.uio.no>
2016-01-03 14:22:58 +0100

    La til waypointsystem med dynamiske waypoints

---
+++ Assets/GameControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameControl : MonoBehaviour {
+
+	public List<GameObject> waypoints = new List<GameObject>();
+	public List<GameObject> stairs = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0;;i++) {
+			GameObject wp = GameObject.Find ("Waypoint " + (i + 1));
+			if (wp == null)
+				break;
+			else
+				waypoints.Add (wp);
+		}
+		for (int i = 0;;i++) {
+			GameObject stair = GameObject.Find ("Stairs " + (i + 1));
+			if (stair == null)
+				break;
+			else
+				stairs.Add (stair);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Stairs.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Stairs : MonoBehaviour {
+	public int floor;
+	public float floorDifference = 1f;
+	private GameControl gameControl;
+	private List<GameObject> stairs = new List<GameObject> ();
+
+	// Use this for initialization
+	void Start () {
+		this.gameControl = GameObject.Find ("GameControl").GetComponent<GameControl>();
+		this.stairs = gameControl.stairs;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MoveDown(GameObject toMove){
+		float newPositionY = transform.position.y-floorDifference;
+		float newPositionX = this.stairs [this.floor - 2].transform.position.x;
+		toMove.transform.position = new Vector3 (newPositionX, newPositionY, 0);
+	}
+
+	public void MoveUp(GameObject toMove){
+		float newPositionY = transform.position.y + floorDifference;
+		float newPositionX = this.stairs [this.floor].transform.position.x;
+		toMove.transform.position = new Vector3 (newPositionX, newPositionY, 0);
+	}
+}
