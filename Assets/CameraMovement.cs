4d55d816292cd6e22a04b96a7f539b6e0fa4b080
PetterV <petter.vilberg@gmail.com>
2016-01-04 10:12:23 +0100

    La inn scripts på kameraet som ikke gjør noe ennå

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Mouse ScrollWheel") > 0){
+			print ("Scroll Up");
+		}
+		if (Input.GetAxis("Mouse ScrollWheel") < 0){
+			print ("Scroll Down");
+		}
+	}
+}
