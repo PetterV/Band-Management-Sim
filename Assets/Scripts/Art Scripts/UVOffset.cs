4456bd2f1c87d84e9f8cacd701c594ad4c174d11
Antler <Eirik.HKI@gmail.com>
2016-01-06 21:49:00 +0100

    Added Skybox + Meta + Scene Changes + Art Scripts
    Hihi dette er egentlig et virus

---
+++ Assets/Scripts/Art Scripts/UVOffset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UVOffset : MonoBehaviour
+{
+    public float scrollSpeed = 0.5F;
+    public Renderer rend;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
