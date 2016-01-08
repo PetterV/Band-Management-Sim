1bfe241598522a7a81c011ec7d86f9869f248df9
PetterV <petter.vilberg@gmail.com>
2016-01-08 15:19:28 +0100

    Hakke gjort noe særlig endringer, men Git mener jeg har det, så...

--- Assets/Scripts/DisplayMoney.cs
+++ Assets/Scripts/DisplayMoney.cs
@@ -5,18 +5,17 @@ using System.Collections;
 public class DisplayMoney : MonoBehaviour {
 
 	private float currentMoney;
-	private string iHaveMoney;
 	private Text moneyDisplay;
 
 	// Use this for initialization
 	void Start () {
-		currentMoney = GameObject.FindWithTag("GameController").GetComponent<GameControl>().penger;
 		moneyDisplay = GameObject.FindWithTag("MoneyDisplay").GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		currentMoney.ToString(iHaveMoney);
-		moneyDisplay.text = iHaveMoney;
+		currentMoney = GameObject.FindWithTag("GameController").GetComponent<GameControl>().penger;
+		string displayMoney = "" + currentMoney;
+		moneyDisplay.text = displayMoney;
 	}
 }
